a3fac8de55ec3bd14fc5bd7eccaf095c3c391017
Jaydeep <jaydipc95@gmail.com>
2022-03-14 18:47:54 +0530

    save

--- my-forms-app/src/app/app.component.ts
+++ my-forms-app/src/app/app.component.ts
@@ -8,7 +8,43 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signUp:NgForm;
+  defaultVal:string = 'pet';
+  answer = '';
+  genders = ['male','female'];
+  user = {
+    username:'',
+    email:'',
+    secret:'',
+    questionAnswr:'',
+    gender:''
+  };
+  submitted = false;
+  
   onSubmit(){
-    console.log(this.signUp);
+    this.submitted =true;
+    this.user.username = this.signUp.value.userData.username;
+    this.user.email = this.signUp.value.userData.email;
+    this.user.secret = this.signUp.value.secret;
+    this.user.questionAnswr = this.signUp.value.questionAnswr;
+    this.user.gender = this.signUp.value.gender;
+
+    this.signUp.reset();
+  }
+  suggestUserName(){
+    const suggestedUser = 'SuperUser';
+    // this.signUp.setValue({
+    //   userData:{
+    //     username:suggestedUser,
+    //     email:''
+    //   },
+    //   secret : 'teacher',
+    //   questionAnswr:'',
+    //   gender : 'male'
+    // });
+    this.signUp.form.patchValue({
+        userData:{
+          username:suggestedUser
+        }
+    });
   }
 }
